07774662c814a706e3acfb2fc8bb12c0adf0815b
César Córdoba <cordobacesar29@gmail.com>
2022-06-08 23:41:11 -0300

    change avatar with dinamic data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+  userName: string;
+  userImg: string;
   public appPages = [
     { title: 'Movies', url: '/movies', icon: 'videocam' },
     { title: 'Favorites', url: '/favorites', icon: 'heart' },
   ];
-  constructor() {}
+  constructor(
+    private authSvc: AuthService,
+  ) {}
+  async getUserName() {
+    this.userName = await this.authSvc.userData.displayName;
+    this.userImg = await this.authSvc.userData.photoURL;
+  }
+  ngOnInit() {
+    setTimeout(() => {
+      this.getUserName();
+    }, 3000);
+  }
 }
