573f30be1decc224ee45b3728d1a49142ee55a9c
César Córdoba <cordobacesar29@gmail.com>
2022-06-11 22:01:10 -0300

    create favorite page

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { MoviesApiService } from '../services/movies-api.service';
+import { Movie } from '../shared/movie.interface';
 
 @Component({
   selector: 'app-favorites',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites.page.scss'],
 })
 export class FavoritesPage implements OnInit {
+  moviesFav: Movie[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private moviesApiService: MoviesApiService,
+    private auth: AuthService
+  ) { }
+  // get favorite movies
+  getFavorites() {
+    this.moviesApiService.getCollection<Movie>('favorites').subscribe(favorites => {
+      this.moviesFav = favorites;
+    });
+  }
+  // delete favorite
+  deleteFavorite(movie: any) {
+    // eslint-disable-next-line no-underscore-dangle
+    this.moviesApiService.deleteDocument('favorites', movie._id);
   }
 
+  ngOnInit(): void {
+    this.auth.presentLoading();
+    setTimeout(() => {
+      this.getFavorites();
+    }, 3000);
+  }
 }
